2a62c2ea11e092ac59ec9f562ca0503fc6c38276
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-08-18 12:59:24 +0600

    error handling

--- app.js
+++ app.js
@@ -13,3 +13,41 @@ function generatePin(){
     const pin = getPin()
     document.getElementById("pinDisplay").value = pin
 }
+
+
+// Calculator body
+document.getElementById("keyPress").addEventListener("click", function(event){
+    const number = event.target.innerText;
+    const displayCalcNumber = document.getElementById("displayCalcNumber")
+
+    if(isNaN(number)){
+        if(number == "C"){
+            displayCalcNumber.value = ""
+        }
+    }else{
+        const currentCalcNumber = displayCalcNumber.value
+        const updatedNumber = currentCalcNumber + number
+
+        displayCalcNumber.value = updatedNumber
+    }
+    
+})
+
+
+
+// Pin verify
+function verifyPin(){
+    const pinDisplay = document.getElementById("pinDisplay").value
+    const displayCalcNumber = document.getElementById("displayCalcNumber").value
+
+
+    const failedMassage = document.getElementById("matched")
+    const success =  document.getElementById("notMatched")
+    if(pinDisplay === displayCalcNumber){
+        failedMassage.style.display = "block"
+        success.style.display = "none"
+    }else{
+        failedMassage.style.display = "block"
+        success.style.display = "none"
+    }
+}
